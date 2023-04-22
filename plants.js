5256a28f8b5612e0eb8c42e887a2f16f0cd50099
celinepastore <ceil.pastore@gmail.com>
2023-04-21 18:03:23 -0700

    Revert "fixed double-nested MapProject"

--- plants.js
+++ plants.js
@@ -8,20 +8,16 @@ plants = {
         "marker-size": "medium",
         "marker-symbol": "garden",
         "name": "Lady Fern",
-          "sciName": "Athyrium filix-femina",
-          "PopupContents": "Athyrium filix-femina",
+        "sciName": "Athyrium filix-femina",
         "link": "https://www.wildernesscollege.com/fiddlehead-ferns.html"
       },
       "geometry": {
         "type": "Point",
-          "coordinates": [-122.31273293495177, 47.6730927122554]
-         }
-      },
-      {"type": "Feature", "properties": {
-          "name": "Audrey II", "Indoor": "yes", "PopupContents": "<div><img src=https://static.wikia.nocookie.net/littleshop/images/2/25/JUMBOAUDREY2.jpg/revision/latest?cb=20150712075657/ width='40' height = '50'>  <p>Hello, Seymour</p> </div>"
-          },
-      
-          "geometry": { "type": "Point", "coordinates": [-122.3070286, 47.6790000] }
+        "coordinates": [
+          -122.31273293495177,
+          47.6730927122554
+        ]
       }
+    }
   ]
 }
