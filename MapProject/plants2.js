df9b98a7f9378f93a995bf5f8b5d5084dbca9695
Celine <ceil.pastore@gmail.com>
2023-06-14 22:36:15 -0700

    adding plants2 file with scc examples; next step add layers

---
+++ MapProject/plants2.js
@@ -0,0 +1,83 @@
+var plants2 = {
+  "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "marker-color": "#008000",
+                "marker-size": "medium",
+                "marker-symbol": "garden",
+                "name": "Lady Fern",
+                "sciName": "Athyrium filix-femina",
+                "description": "Fiddle heads are edible.",
+                "PopupContents": "Athyrium filix-femina",
+                "link": "https://www.wildernesscollege.com/fiddlehead-ferns.html",
+                "location": "Home"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-122.31273293495177, 47.6730927122554]
+            }
+        },
+        {
+            "type": "Feature", "properties": {
+                "name": "Audrey II", "Indoor": "yes", "PopupContents": "<div><img src=https://static.wikia.nocookie.net/littleshop/images/2/25/JUMBOAUDREY2.jpg/revision/latest?cb=20150712075657/ width='40' height = '50'>   </div>"
+                , "description": "Hello, Seymour"
+                , "location": "Home"
+            },
+
+            "geometry": { "type": "Point", "coordinates": [-122.3070286, 47.6790000] }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Pyramidal White Birch",
+                "sciName": "Betula pendula fastigata",
+                "num": "52",
+                "family": "Betulaceae",
+                "description": "Fastigata refersto the more erect form ofthis birch, which often has a more pendulous form from which its species name is derived.The white bark with horizontal dark lines and black diamond- shaped patches on the trunk are typical.",
+                "PopupContents": "Betula pendula fastigata",
+                "link": "https://plants.usda.gov/home/plantProfile?symbol=BEPE3",
+                "location": "Shoreline"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-122.359818, 47.747974, ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Winter Jasmine",
+                "sciName": "Jasminum nudiflorum",
+                "num": "13",
+                "family": "Oleaceae",
+                "description": "One of the first plants to bloom on the campus, usually in January before the leaves show. The small yellow flowers are not fragrant, unlike many Jasmines.",
+                "PopupContents": "Jasminum nudiflorum",
+                "link": "https://plants.usda.gov/home/plantProfile?symbol=JANU",
+                "location": "Shoreline"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-122.35889, 47.748342]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Alpine Fir",
+                "sciName": "Abies lasiocarpa",
+                "num": "39",
+                "family": "Pinaceae",
+                "PopupContents": "Abies lasiocarpa",
+                "description": "Needles on this fir are spirally arranged, tightly together and nearly erect, on the upper side ofthe twigs.The beautiful shape and slow growing nature of this fir make it ideal as a bonsai. This shape also allows it to withstand the heavy snow loads in its sub-alpine habitat.",
+                "link": "https://plants.usda.gov/home/plantProfile?symbol=BEPE3",
+                "location": "Shoreline"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-122.360666, 47.749215]
+            }
+        }
+    ]
+}
